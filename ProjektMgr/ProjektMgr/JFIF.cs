0b9faabb26d998125209e8fa5756274659f78afd
MichalKupczyk1 <72615252+MichalKupczyk1@users.noreply.github.com>
2024-10-08 01:19:00 +0200

    Dodanie kwantyzacji, DCT oraz zigzag
    Dodanie kwantyzacji, DCT oraz zigzag

--- ProjektMgr/ProjektMgr/JFIF.cs
+++ ProjektMgr/ProjektMgr/JFIF.cs
@@ -9,12 +9,29 @@ namespace ProjektMgr
     //jpeg jfif
     public static class JFIF
     {
+        //domyslnie 8x8, wedlug standardu jfif
+        private static int blockSize = 8;
         public static RGBPixel[,] Pixels { get; set; }
         public static void LoadAllPixels(byte[] bytes, long width, long height, int padding)
         {
             var oneDimArray = CreateOneDimRGBPixelArray(bytes, width, height, padding);
             var YCbCrArray = CreateTwoDimYCbCrPixelArray(oneDimArray, width, height);
             var downsampledArray = ApplyDownsampling(YCbCrArray, width, height);
+            var Y = ShiftValuesInArray(ExtendArrayIfNeeded(downsampledArray.Y));
+            var Cr = ShiftValuesInArray(ExtendArrayIfNeeded(downsampledArray.Cr));
+            var Cb = ShiftValuesInArray(ExtendArrayIfNeeded(downsampledArray.Cb));
+
+            var YDCT = ApplyDCTToBlock(Y)
+                .Select(x => QuantizeYBlock(x))
+                .ToList();
+
+            var CrDCT = ApplyDCTToBlock(Cr)
+                .Select(x => QuantizeCbCrBlock(x))
+                .ToList();
+
+            var CbDCT = ApplyDCTToBlock(Cb)
+                .Select(x => QuantizeCbCrBlock(x))
+                .ToList();
         }
 
         private static RGBPixel[] CreateOneDimRGBPixelArray(byte[] bytes, long width, long height, int padding)
@@ -78,5 +95,178 @@ namespace ProjektMgr
 
             return (Y, Cb, Cr);
         }
+
+        private static float[,] ExtendArrayIfNeeded(float[,] array)
+        {
+            int originalRows = array.GetLength(0);
+            int originalCols = array.GetLength(1);
+
+            int rowsToAdd = (originalRows % 8 != 0) ? 8 - (originalRows % 8) : 0;
+            int colsToAdd = (originalCols % 8 != 0) ? 8 - (originalCols % 8) : 0;
+
+            if (rowsToAdd == 0 && colsToAdd == 0)
+                return array;
+
+            float[,] newArray = new float[originalRows + rowsToAdd, originalCols + colsToAdd];
+
+            for (int i = 0; i < newArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < newArray.GetLength(1); j++)
+                {
+                    if (i >= originalRows)
+                        newArray[i, j] = array[originalRows - 1, Math.Min(j, originalCols - 1)];
+
+                    else if (j >= originalCols)
+                        newArray[i, j] = array[i, originalCols - 1];
+
+                    else
+                        newArray[i, j] = array[i, j];
+                }
+            }
+
+            return newArray;
+        }
+
+        private static float[,] CalculateDCT(float[,] block)
+        {
+            var width = block.GetLength(0);
+            var height = block.GetLength(1);
+            var result = new float[width, height];
+
+            for (int u = 0; u < blockSize; u++)
+            {
+                for (int v = 0; v < blockSize; v++)
+                {
+                    float sum = 0.0f;
+
+                    for (int x = 0; x < blockSize; x++)
+                        for (int y = 0; y < blockSize; y++)
+                            sum += block[x, y] * (float)Math.Cos(((2 * x + 1) * u * Math.PI) / (2 * blockSize)) * (float)Math.Cos(((2 * y + 1) * v * Math.PI) / (2 * blockSize));
+
+                    float alphaU = (u == 0) ? (float)(1 / Math.Sqrt(2)) : 1.0f;
+                    float alphaV = (v == 0) ? (float)(1 / Math.Sqrt(2)) : 1.0f;
+                    result[u, v] = 0.25f * alphaU * alphaV * sum;
+                }
+            }
+            return result;
+        }
+
+        public static float[,] ShiftValuesInArray(float[,] array)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    array[i, j] -= 128;
+                }
+            }
+            return array;
+        }
+
+        private static List<float[,]> ApplyDCTToBlock(float[,] array)
+        {
+            int width = array.GetLength(0);
+            int height = array.GetLength(1);
+            var res = new List<float[,]>();
+
+            for (int i = 0; i < height; i += blockSize)
+            {
+                for (int j = 0; j < width; j += blockSize)
+                {
+                    var block = new float[blockSize, blockSize];
+                    for (int x = 0; x < blockSize; x++)
+                    {
+                        for (int y = 0; y < blockSize; y++)
+                        {
+                            block[x, y] = array[i + x, j + y];
+                        }
+                    }
+                    var dct = CalculateDCT(block);
+                    res.Add(dct);
+                }
+            }
+            return res;
+        }
+
+        private static float[,] QuantizeYBlock(float[,] block)
+        {
+            var res = new float[blockSize, blockSize];
+
+            for (int i = 0; i < blockSize; i++)
+            {
+                for (int j = 0; j < blockSize; j++)
+                    res[i, j] = (float)Math.Round(block[i, j] / CalculationArrays.QuantizationYTable[i, j]);
+            }
+            return res;
+        }
+
+        private static float[,] QuantizeCbCrBlock(float[,] block)
+        {
+            var res = new float[blockSize, blockSize];
+
+            for (int i = 0; i < blockSize; i++)
+            {
+                for (int j = 0; j < blockSize; j++)
+                    res[i, j] = (float)Math.Round(block[i, j] / CalculationArrays.QuantizationCbCrTable[i, j]);
+            }
+            return res;
+        }
+
+        private static int[] ZigZagScan(float[,] block)
+        {
+            var res = new int[64];
+
+            for (int i = 0; i < 64; i++)
+            {
+                int x = CalculationArrays.ZigZagScan[i] / 8;
+                int y = CalculationArrays.ZigZagScan[i] % 8;
+                res[i] = (int)block[x, y];
+            }
+
+            return res;
+        }
+    }
+
+    public static class CalculationArrays
+    {
+        //all tables according to jfif/jpeg standard
+        public static readonly int[,] QuantizationYTable = new int[8, 8]
+            {
+                { 16, 11, 10, 16, 24, 40, 51, 61 },
+                { 12, 12, 14, 19, 26, 58, 60, 55 },
+                { 14, 13, 16, 24, 40, 57, 69, 56 },
+                { 14, 17, 22, 29, 51, 87, 80, 62 },
+                { 18, 22, 37, 56, 68, 109, 103, 77 },
+                { 24, 35, 55, 64, 81, 104, 113, 92 },
+                { 49, 64, 78, 87, 103, 121, 120, 101 },
+                { 72, 92, 95, 98, 112, 100, 103, 99 }
+            };
+
+        public static readonly int[,] QuantizationCbCrTable = new int[8, 8]
+            {
+                { 17, 18, 24, 47, 99, 99, 99, 99 },
+                { 18, 21, 26, 66, 99, 99, 99, 99 },
+                { 24, 26, 56, 99, 99, 99, 99, 99 },
+                { 47, 66, 99, 99, 99, 99, 99, 99 },
+                { 99, 99, 99, 99, 99, 99, 99, 99 },
+                { 99, 99, 99, 99, 99, 99, 99, 99 },
+                { 99, 99, 99, 99, 99, 99, 99, 99 },
+                { 99, 99, 99, 99, 99, 99, 99, 99 }
+            };
+
+        public static readonly int[] ZigZagScan = new int[64]
+             {
+                0, 1, 5, 6, 14, 15, 27, 28,
+                2, 4, 7, 13, 16, 26, 29, 42,
+                3, 8, 12, 17, 25, 30, 41, 43,
+                9, 11, 18, 24, 31, 40, 44, 53,
+                10, 19, 23, 32, 39, 45, 52, 54,
+                20, 22, 33, 38, 46, 51, 55, 60,
+                21, 34, 37, 47, 50, 56, 59, 61,
+                35, 36, 48, 49, 57, 58, 62, 63
+             };
     }
 }
